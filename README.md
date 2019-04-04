c2dde12308c034c5d32436974f6dd0865ae69564
amartinr1977 <amartinromero@gmail.com>
2019-04-04 20:00:33 +0200

    ok

---
+++ README.md
@@ -0,0 +1,3 @@
+# APP Lista de la Compra mediante Vue.js y Router Vue
+
+Se trata de una Sigle Page APP hecha en Vue.js y Vue Router
---
+++ js/main.js
@@ -0,0 +1,131 @@
+Vue.component("comp-elementolista", {
+  props: ["producto"],
+  template: "#plantilla-com-elementolista",
+  methods: {
+    CambiarEstilo() {
+      this.$emit("clickcheck");
+    },
+    EliminarElemento() {
+      this.$emit("clickdelete");
+    }
+  }
+});
+
+Vue.component("barra-progreso", {
+  template: "#plantilla-barra-progreso",
+  props: ["porcentaje"]
+});
+
+Vue.component("mensaje-error", {
+  template: "#plantilla-mensaje-error"
+});
+
+const ListadoCompra = Vue.component("comp-listado-compra", {
+  template: "#t-listado-compra",
+  data() {
+    return {
+      titulo: "Lista de la Compra con Vue.js",
+      articulo: "",
+      cantidad: 0,
+      prioridad: "Baja",
+      listado: []
+    };
+  },
+  computed: {
+    ComprasHechas() {
+      return this.listado.filter(elemento => elemento.estado);
+    },
+    Porcentaje() {
+      return ((this.ComprasHechas.length * 100) / this.listado.length).toFixed(
+        2
+      );
+    },
+    PorcentajeEnTantoPorCien() {
+      return this.Porcentaje.toString() + "%";
+    }
+  },
+  methods: {
+    Agregar() {
+      elemento = {
+        art: this.articulo,
+        can: this.cantidad,
+        pri: this.prioridad,
+        estado: false,
+        id: Math.random()
+          .toString()
+          .substring(2, 9)
+      };
+      this.listado.push(elemento);
+      console.log(this.listado);
+      this.ResetValores();
+    },
+    ResetValores() {
+      this.articulo = "";
+      this.cantidad = 0;
+      this.prioridad = "Baja";
+    },
+    Eliminar(item) {
+      indice = this.listado.indexOf(item);
+      this.listado.splice(indice, 1);
+    },
+    CambiarEstado(item) {
+      item.estado = !item.estado;
+    }
+  }
+});
+
+const ListadoComprasHechas = Vue.component("comp-listado-comprashechas", {
+  data() {
+    return {
+      titulo: "Lista de Compras Hechas"
+    };
+  },
+  /*html*/
+  template: `
+  <div>
+    <div class="row cabecera justify-content-center mt-5" id="cabecera">
+    <h1 id="titulo1">
+      <i class="material-icons" style="font-size: 1em">shopping_cart</i>
+      {{ titulo }}
+    </h1>
+    </div>
+  </div>
+  `
+});
+
+const AcercaDe = Vue.component("comp-acercade", {
+  data() {
+    return {
+      titulo: "Información del Sitio Web"
+    };
+  },
+  /*html*/
+  template: `
+  <div>
+    <div class="row cabecera justify-content-center mt-5" id="cabecera">
+    <h1 id="titulo1">
+      <i class="material-icons" style="font-size: 1em">shopping_cart</i>
+      {{ titulo }}
+    </h1>
+    </div>
+  </div>
+  `
+});
+
+const misrutas = [
+  { path: "/", component: ListadoCompra },
+  { path: "/miscompras", component: ListadoComprasHechas },
+  { path: "/info", component: AcercaDe }
+];
+
+const router = new VueRouter({
+  routes: misrutas
+});
+
+const miapp = new Vue({
+  router,
+  el: "#contenedor",
+  data: {},
+  computed: {},
+  methods: {}
+});
